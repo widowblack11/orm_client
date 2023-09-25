657742aa270aaf392509686099ee97a412035ffc
widowblack11 <o.prokopenko@notamedia.com.ru>
2023-09-25 20:33:20 +0300

    orm client

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ REQUIRES = [
 setup(
     name='orm_client',
     version='0.0.1',
-    packages=['ormclient'],
+    packages=['orm_client'],
     url='https://github.com/widowblack11/orm_client.git',
     license='MIT',
     author='oksanaprokopenko',
